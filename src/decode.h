77914d06b7245ec085e33d9e2992325d77887754
Tom DeCanio <td@npulsetech.com>
2013-05-03 12:59:33 -0700

    fix netmap compile errors on BSD

--- src/decode.h
+++ src/decode.h
@@ -60,6 +60,7 @@ enum PktSrcEnum {
 #include "source-pcap.h"
 #include "source-af-packet.h"
 #include "source-mpipe.h"
+#include "source-netmap.h"
 
 #include "action-globals.h"
 
@@ -398,6 +399,9 @@ typedef struct Packet_
 #ifdef HAVE_MPIPE
         /* tilegx mpipe stuff */
         MpipePacketVars mpipe_v;
+#endif
+#ifdef HAVE_NETMAP
+        NetmapPacketVars netmap_v;
 #endif
 
         /** libpcap vars: shared by Pcap Live mode and Pcap File mode */
